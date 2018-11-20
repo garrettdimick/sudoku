cb7e2bc9039cc3416fbf7395a7063d9c8c7e088c
garrettdimick <garrett.dimick@gmail.com>
2018-11-19 20:59:27 -0700

    Finished

--- test/HugeSolverTest.java
+++ test/HugeSolverTest.java
@@ -8,6 +8,12 @@ public class HugeSolverTest {
 
     @Test
     public void nextValuesTest() {
+
+        System.out.println(hs.nextValues());
+    }
+
+    @Test
+    public void reduceListTest() {
         char[][] board = {
                 {'4', '9', '-', '1', '3', '6', '7', '-', '8'},
                 {'-', '6', '3', '5', '-', '-', '-', '9', '-'},
@@ -24,22 +30,24 @@ public class HugeSolverTest {
         this.dimension = 9;
         this.symbols = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
         SudokuSolver hs = new HugeSolver(sg);
-        hs.nextValues();
-    }
-
-    @Test
-    public void reduceListTest() {
-    }
-
-    @Test
-    public void getAppearancesTest() {
+        assertEquals(hs.reduceList(sg.symbols, 0, 2),[2]);
     }
 
     @Test
     public void updateCountTest() {
-    }
+        char[][] board = {
+                {'2', '-', '3', '1'},
+                {'1', '3', '-', '4'},
+                {'3', '1', '4', '-'},
+                {'-', '2', '1', '3'}
 
-    @Test
-    public void sortByAppearanceTest() {
+        };
+        SudokuGame sg = new SudokuGame();
+        this.currentBoard = board;
+        this.dimension = 9;
+        this.symbols = ['1', '2', '3', '4'];
+        SudokuSolver hs = new HugeSolver(sg);
+        hs.updateCount('4');
+        assertEquals(hs.appearances.get('4'), 3);
     }
 }
